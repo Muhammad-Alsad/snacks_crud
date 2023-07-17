8fc313a43c0c998b290b385803906c524925363e
muhammad <moalsad001@gmail.com>
2023-07-17 19:56:20 +0000

    2nd

---
+++ snacks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Snack(models.Model):
+    title = models.CharField(max_length=255, help_text='Snack title')
+    description  = models.TextField(max_length=255,help_text="Descriptions of the snack",default="no description") 
+    purchaser  = models.ForeignKey(get_user_model(), on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.title
+   
