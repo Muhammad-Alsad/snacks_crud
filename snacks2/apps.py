dbab7e7716045ebd4f853275ff9f6c3276adbc4e
muhammad <moalsad001@gmail.com>
2023-07-18 13:20:45 +0000

    snacks2

--- snacks2/apps.py
+++ snacks2/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SnacksConfig(AppConfig):
+class Snacks2Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'snacks'
+    name = 'snacks2'
--- snacks_crud_project/urls.py
+++ snacks_crud_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('snacks.urls'))
+    # path('',include('snacks.urls'))
+    path('',include('snacks2.urls'))
 ]
